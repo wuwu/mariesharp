0400922056c1dd0fb90ad14eef0087936eddbf77
Florian Ewald <wuwu@gmx.net>
2018-02-19 14:00:00 +0100

    not fucking working!!!

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -14,7 +14,7 @@ module.exports = {
       'products',
       {
         page: '/shop/_slug',
-        permalink: "/shop/:slug",
+        permalink: "/:slug",
         generate: ['get', 'getAll'],
         isPost: false,
       }]
