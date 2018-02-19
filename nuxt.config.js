9e62005c59e9517d5910d9056906acc95dd068d6
Florian Ewald <wuwu@gmx.net>
2018-02-19 17:52:41 +0100

    config api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,10 +115,15 @@ module.exports = {
       ],
       ["products", {
         page: '/shop/_product',
-        permalink: "/shop/:slug",
+        permalink: '/shop/:slug',
         isPost: false,
         generate: ['get', 'getAll'],
       }]
-    ]
+    ],
+    api: {
+      baseURL: process.env.NODE_ENV === 'production'
+          ? 'https://mariesharp.zknft.de'
+          : 'http://localhost:4500'
+    }
   }
 }
